993209e969fb7bc94c40da31a6cd5faea3cf3446
mogesTesema <moges2421@gmail.com>
2026-02-05 20:55:52 +0300

    environment set up finished,playground app created

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nb+6d+80=r_e0(z^$kys@n@i1%c@iyag6%*47b@f37%$dyy_1z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playground'
 ]
 
 MIDDLEWARE = [
