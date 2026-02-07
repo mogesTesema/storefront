8f654e72d0594a0a75e53d28fd9e4d88edfa511b
mogesTesema <moges2421@gmail.com>
2026-02-07 16:45:18 +0300

    mysql configuration set up

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,32 @@
 FROM python:3.12-slim
 
+# Environment variables (rarely change)
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 
+# Set working dir
 WORKDIR /app
 
+# Install system dependencies (rarely change)
+RUN apt-get update && apt-get install -y \
+    default-libmysqlclient-dev \
+    build-essential \
+    pkg-config \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install Python tools (rarely change)
 RUN pip install --no-cache-dir uv
 
-# copy lockfile first (cache-friendly)
+# Copy requirements first
 COPY requirements.lock .
+
+# Install Python dependencies
 RUN uv pip install --system -r requirements.lock
 
-# then copy the rest
+# Copy project source last (frequent changes)
 COPY . .
+
+# expose port
+EXPOSE 8000
+# run command
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,9 +80,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": int(os.environ.get("DB_PORT", 3306)),
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
