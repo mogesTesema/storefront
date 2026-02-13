945fa20b86819cb125d195596ec13035ba69be66
mogesTesema <moges2421@gmail.com>
2026-02-13 07:22:25 +0300

    djangorestfulapi installed,data model migrated to new database storefront2

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME"),
+        "NAME": os.environ.get("DB_NAME2"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
