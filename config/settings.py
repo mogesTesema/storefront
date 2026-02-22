51044d97c35ea47082c70a4e3b93d2d311aea1af
mogesTesema <moges2421@gmail.com>
2026-02-22 21:53:58 +0300

    fake email sended setted up

--- config/settings.py
+++ config/settings.py
@@ -200,6 +200,13 @@ SIMPLE_JWT = {
 
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'from@moshbuy.com'
+
 
 
 LOGGING = {
