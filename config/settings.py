9de6d3e383d51105a4166bbcdd1de2199ed77971
mogesTesema <moges2421@gmail.com>
2026-02-22 10:02:02 +0300

    new database created for third part of project section, custom command created, env setted up

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME2"),
+        "NAME": os.environ.get("DB_NAME3"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
