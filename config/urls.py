3014dad085302b364dff0fc54c0f735f6bf0ca94
mogesTesema <moges2421@gmail.com>
2026-02-22 10:57:56 +0300

    media endpoint created

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings
+
 
 admin.site.site_header = "Storefront Admin"
 admin.site.index_title = "Admin"
@@ -27,11 +29,8 @@ urlpatterns = [
     path('store/',include('store.urls')),
     path('auth/',include('djoser.urls')),
     path('auth/',include('djoser.urls.jwt'))
-]
+] 
 
 
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns = [
-#         path("__debug__/", include(debug_toolbar.urls)),
-#     ] + urlpatterns
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
